0bada6e3866daeaf519777580e14a28b716808e6
Fabien <fabien.marcuccini@etu.univ-amu.fr>
2024-09-25 17:31:27 +0200

    fix public ImplicationGraph mirror () throws Exception add private void addReversedEdges(ImplicationGraph mirror, int index) throws Exception fix public class ImplicationGraph extends Graph<Integer>

--- src/ImplicationGraph.java
+++ src/ImplicationGraph.java
@@ -1,17 +1,21 @@
-import java.util.LinkedList;
-
-public class ImplicationGraph extends Graph {
+public class ImplicationGraph extends Graph<Integer> {
 
     public ImplicationGraph(int size) {
         super(size);
     }
 
-    ImplicationGraph mirror () {
-        ImplicationGraph mirror = new ImplicationGraph(super.cardinal);
+    public ImplicationGraph mirror () throws Exception {
+        ImplicationGraph mirror = new ImplicationGraph(super.getCardinal());
 
-        for (int index = 0; index < super.cardinal; index++){
-
+        for (int index = 0; index < super.getCardinal(); index++){
+            addReversedEdges(mirror, index);
         }
         return mirror;
+    }
+
+    private void addReversedEdges(ImplicationGraph mirror, int index) throws Exception {
+        for(Edge edge : getEdges(index)) {
+            mirror.addEdge(edge.destination, edge.source, edge.label);
+        }
     }
 }
