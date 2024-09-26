6c28d2bee8da63708e11bcca39d8937914e05e06
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-09-26 11:35:54 +0200

    add Parser class

---
+++ src/Parser.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Parser {
+    /**
+     * Builds the graph related to the file given.
+     * @param filename the file to read
+     */
+    public ImplicationGraph buildImplicationGraph(String filename) {
+
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
+            bufferedReader.readLine();
+            String[] cardinalLine = bufferedReader.readLine().split(" ");
+            int size = 2 * Integer.parseInt(cardinalLine[2]);
+            ImplicationGraph implicationGraph = new ImplicationGraph(size);
+            fillGraph(bufferedReader, implicationGraph);
+            System.out.println(implicationGraph);
+
+            return implicationGraph;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Fills the graph with the edges appearing on the file.
+     * @param bufferedReader the bufferedReader to read the file.
+     * @param implicationGraph the graph we want to fill.
+     * @throws Exception bufferedReader exception
+     */
+    private static void fillGraph(BufferedReader bufferedReader,ImplicationGraph implicationGraph) throws Exception {
+        while ((bufferedReader.readLine()) != null) {
+            String[] clauseLine = bufferedReader.readLine().split(" ");
+            implicationGraph.addEdgesFromClause(clauseLine);
+        }
+    }
+}
