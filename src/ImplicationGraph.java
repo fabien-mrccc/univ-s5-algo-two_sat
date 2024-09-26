1801211626258ba532e34381634704e1c35a28b8
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-09-26 11:33:33 +0200

    remove public ImplicationGraph mirror () throws Exception remove private void addReversedEdges(ImplicationGraph mirror, int index) throws Exception add public void addEdgesFromClause(String[] clauseLine) throws Exception add private int getLiteralIndex(int literal) add protected int getLiteral(int index) add public String toString()

--- src/ImplicationGraph.java
+++ src/ImplicationGraph.java
@@ -7,19 +7,49 @@ public class ImplicationGraph extends Graph<Integer> {
         super(size);
     }
 
-    public ImplicationGraph mirror () throws Exception {
-        ImplicationGraph mirror = new ImplicationGraph(super.getCardinal());
-
-        for (int index = 0; index < super.getCardinal(); index++){
-            addReversedEdges(mirror, index);
-        }
-        return mirror;
+    public void addEdgesFromClause(String[] clauseLine) throws Exception {
+        addEdge(getLiteralIndex(- Integer.parseInt(clauseLine[0])),
+                getLiteralIndex(Integer.parseInt(clauseLine[1])),Integer.parseInt(clauseLine[2]));
+        addEdge(getLiteralIndex(- Integer.parseInt(clauseLine[1])),
+                getLiteralIndex(Integer.parseInt(clauseLine[0])),Integer.parseInt(clauseLine[2]));
     }
 
-    private void addReversedEdges(ImplicationGraph mirror, int index) throws Exception {
-        for(Edge edge : getEdges(index)) {
-            mirror.addEdge(edge.destination, edge.source, edge.label);
+    /**
+     * Return the index of the literal we gave in parameters.
+     * @param literal the literal we want the index of.
+     * @return the index of the literal
+     */
+    private int getLiteralIndex(int literal){
+        if(literal < 0){
+            return literal + getCardinal()/2;
         }
+        return literal + getCardinal()/2 -1;
+    }
+
+    protected int getLiteral(int index){
+        if(index < getCardinal()/2){
+            return index - getCardinal()/2;
+        }
+        return index - getCardinal()/2 +1;
+    }
+
+    public String toString() {
+        String result = "";
+        result = result.concat(STR."Nombre sommets : \{getCardinal()}\n");
+        result = result.concat("Litteral/Index : \n");
+
+        for (int i = 0; i<getCardinal();i++) {
+            result = result.concat(STR."\{getLiteral(i)}/\{i} ");
+        }
+
+        result = result.concat("\nArcs : \n");
+
+        for (int i = 0; i<getCardinal();i++) {
+            for (Edge e : getEdges(i)) {
+                result = result.concat(STR."\{getLiteral(e.source)} -> \{getLiteral(e.destination)}, étiquette : \{e.label.toString()}\n");
+            }
+        }
+        return result;
     }
 
     /**
@@ -27,6 +57,7 @@ public class ImplicationGraph extends Graph<Integer> {
      * means all its neighbors are visited, the vertex is pushed onto the processingOrderStack.
      * @return the processing order stack.
      */
+    //TODO: change stack par queue et mettre le code dans une autre classe (eventuellement kosaraju)
     public Stack<Integer> getVertexProcessingOrder(){
         Stack<Integer> processingOrderStack = new Stack<>();
         boolean[] visitedVertexes = new boolean[getCardinal()];
@@ -47,6 +78,7 @@ public class ImplicationGraph extends Graph<Integer> {
         }
         processingOrderStack.push(index); // Empiler le nœud après avoir visité tous ses voisins
     }
+
 
 
 }
