d3bdbab63d7982dfc81948a6557e0648924829ce
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-09-26 11:35:36 +0200

    fix public static void main(String[] args) remove private static void buildImplicationGraph(String[] args) remove private static ImplicationGraph initializeGraph(int size) remove private static void fillGraph(BufferedReader bufferedReader,ImplicationGraph implicationGraph) throws Exception remove private static void changeClauseIntoEdges(ImplicationGraph implicationGraphs, String[] clauseLine) throws Exception remove private static int getVertexIndex(int vertex, int size)

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static java.lang.System.exit;
 
 public class Main {
 
     public static void main(String[] args) {
+        String filename = "formulas/formula.txt";
+        if (0 < args.length) {
+            filename = args[0];
+        }
 
-        buildImplicationGraph(args);
+        Parser graphParser = new Parser();
+        ImplicationGraph implicationGraph = graphParser.buildImplicationGraph( filename);
 
-        /*Parser parser = new Parser();
+        /*
         Graph<String> graph = parser.parse(filename);
 
         Kosaraju k = new Kosaraju(graph);
@@ -27,73 +28,5 @@ public class Main {
             exit(-1);
         }*/
         exit(0);
-    }
-
-    /**
-     * Builds the graph related to the file given.
-     * @param args
-     */
-    private static void buildImplicationGraph(String[] args) {
-        String filename = "formulas/formula.txt";
-        if (0 < args.length) {
-            filename = args[0];
-        }
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filename))) {
-            bufferedReader.readLine();
-            String[] cardinalLine = bufferedReader.readLine().split(" ");
-            int size = 2* Integer.parseInt(cardinalLine[2]);
-            ImplicationGraph implicationGraph = initializeGraph(size);
-            fillGraph(bufferedReader, implicationGraph);
-            System.out.println(implicationGraph);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    /**
-     * Initialize the graph with a length equal to size.
-     * @param size the necessary length to stock all the edges
-     * @return the graph
-     */
-    private static ImplicationGraph initializeGraph(int size){
-        return new ImplicationGraph(size);
-    }
-
-    /**
-     * Fills the graph with the edges appearing on the file.
-     * @param bufferedReader the bufferedReader to read the file.
-     * @param implicationGraph the graph we want to fill.
-     * @throws Exception
-     */
-    private static void fillGraph(BufferedReader bufferedReader,ImplicationGraph implicationGraph) throws Exception {
-        while ((bufferedReader.readLine()) != null) {
-            String[] clauseLine = bufferedReader.readLine().split(" ");
-            changeClauseIntoEdges(implicationGraph, clauseLine);
-        }
-    }
-
-    private static void changeClauseIntoEdges(ImplicationGraph implicationGraphs, String[] clauseLine) throws Exception {
-        implicationGraphs.addEdge(getVertexIndex(- Integer.parseInt(clauseLine[0]), implicationGraphs.getCardinal()),
-                getVertexIndex(Integer.parseInt(clauseLine[1]), implicationGraphs.getCardinal()),clauseLine[2]);
-        implicationGraphs.addEdge(getVertexIndex(- Integer.parseInt(clauseLine[1]), implicationGraphs.getCardinal()),
-                getVertexIndex(Integer.parseInt(clauseLine[0]), implicationGraphs.getCardinal()),clauseLine[2]);
-    }
-
-    /**
-     * Return the index of the vertex we gave in parameters.
-     * @param vertex the vertex we want the index of.
-     * @param size the size of the graphe
-     * @return the index of the vertex
-     */
-    private static int getVertexIndex(int vertex, int size){
-        if(vertex < 0){
-            return vertex + size/2;
-        }
-        return vertex + size/2 -1;
     }
 }
