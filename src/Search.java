78480f50a8dc0e4893598fc1d3d25c52621a4a41
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-10-02 16:59:17 +0200

    add comment to public void explore(Graph<Label>.Edge edge) add comment to public LinkedList<Graph<Label>.Edge> iterativeDFS()

--- src/Search.java
+++ src/Search.java
@@ -1,38 +1,47 @@
-import java.lang.classfile.Label;
 import java.util.LinkedList;
 
 public class Search {
-    private final Graph<Label> graph;
+    private final Graph<Integer> graph;
 
-    private LinkedList<Graph<Label>.Edge> predecessor;
+    private final LinkedList<Graph<Integer>.Edge> predecessor;
 
-    private LinkedList<Integer> visitedIndex;
+    private final LinkedList<Integer> visitedIndex;
 
-    public Search(Graph<Label> graph){
+    public Search(Graph<Integer> graph){
         this.graph = graph;
         this.predecessor = new LinkedList<>();
         this.visitedIndex = new LinkedList<>();
     }
 
-    public void explore(Graph<Label>.Edge edge){
+    /**
+     * Recursively explores the graph from the given edge, marking visited indexes and
+     * recording their predecessors.
+     * @param edge the edge from which begin the exploration.
+     */
+    public void explore(Graph<Integer>.Edge edge){
         int destinationIndex = edge.destination;
         if(!visitedIndex.contains(destinationIndex)){
             visitedIndex.add(destinationIndex);
             predecessor.set(destinationIndex, edge);
         }
 
-        for( Graph<Label>.Edge destinationEdge : graph.getIncidents(destinationIndex)){
+        for( Graph<Integer>.Edge destinationEdge : graph.getIncidents(destinationIndex)){
             explore(destinationEdge);
         }
     }
 
-    public LinkedList<Graph<Label>.Edge> iterativeDFS(){
+    /**
+     * Performs an iterative depth-first search (DFS) on the graph, marking visited indexes
+     * and recording their predecessors, and returns the predecessor list.
+     * @return the predecessor list.
+     */
+    public LinkedList<Graph<Integer>.Edge> iterativeDFS(){
         for(int index : graph.getIndexes()){
             if(!visitedIndex.contains(index)){
                 visitedIndex.add(index);
                 predecessor.set(index,null);
             }
-            for(Graph<Label>.Edge edge : graph.getIncidents(index)){
+            for(Graph<Integer>.Edge edge : graph.getIncidents(index)){
                 explore(edge);
             }
         }
