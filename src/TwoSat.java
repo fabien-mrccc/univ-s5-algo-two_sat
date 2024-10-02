e94d4d129ee49a409375760b4be005bf644a6b44
aminafanani <amina.fanani@etu.univ-amu.fr>
2024-10-02 18:40:59 +0200

    add TwoSat.java

---
+++ src/TwoSat.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class TwoSat {
+    //TODO: à partir de destinationIndex remonter jusqu'en haut et voir s'il y'a son opposé (trouver
+    // une meilleure methode)
+    public static boolean checkConsistency(LinkedList<Graph<Integer>.Edge> predecessors){
+
+        for(Graph<Integer>.Edge edge : predecessors){
+
+        }
+        return true;
+    }
+}
