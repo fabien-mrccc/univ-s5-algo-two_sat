de4e5cf1e32878edb5a32364fe4462d1fa537d94
fabien-mrccc <fabien.marcuccini@etu.univ-amu.fr>
2024-10-16 18:55:35 +0200

    add //TODO: make Edge a specific class

--- src/Graph.java
+++ src/Graph.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 
 public class Graph<Label>  {
 
+    //TODO: make Edge a specific class
     public class Edge {
         public int source;
         public int destination;
